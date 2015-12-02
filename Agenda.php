56ac7056e1983ceb4d6deacb4e7083c04020e3de
Rigterink <rigterinkcris@gmail.com>
2015-12-02 10:31:36 +0100

    agenda voorlopig af

--- Agenda.php
+++ Agenda.php
@@ -7,10 +7,10 @@
     	<header>
     	<?php include 'header.php'; ?>
     	</header>
-               
-        </div>
         <div class="info">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=gray&amp;src=j3ft4po2nu6490gg7q8g67dmjk%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=Europe%2FAmsterdam" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            <div class="agenda">
+        <iframe src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=grey&amp;src=j3ft4po2nu6490gg7q8g67dmjk%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=Europe%2FAmsterdam" style="border-width:0" width="600" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
     </div>
     </body>
 </html>
