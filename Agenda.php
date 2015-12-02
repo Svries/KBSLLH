ab5946322c39a1800d42c53c72d7e716dbadb870
Rigterink <rigterinkcris@gmail.com>
2015-12-02 09:41:08 +0100

    google agenda toegevoegd

--- Agenda.php
+++ Agenda.php
@@ -9,6 +9,8 @@
     	</header>
                
         </div>
-        <div class="info"></div>
+        <div class="info">
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffcc&amp;src=j3ft4po2nu6490gg7q8g67dmjk%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=Europe%2FAmsterdam" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
     </body>
 </html>
