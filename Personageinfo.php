56e0740d9729d9e52787df24cd4cd64f54d3ff66
sebastiaan_vries <vries.sebastiaan@gmail.com>
2015-12-02 11:09:30 +0100

    Changes in personage

---
+++ Personageinfo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <?php include 'head.php';?>
+    </head>
+    
+    <body> 
+        <header>
+            <?php include 'header.php'; ?>
+        </header>
+
+        <div class="info">
+            <div class="infopersonage">
+                <h2> Hans Kazan </h2>
+                    <img class="personagefoto" src="http://shackmanlab.org/wp-content/uploads/2013/07/person-placeholder.jpg">
+                    <p> Verhaaltje............................................................................................................................................................................................................................................................................................................................</p>
+            </div>
+        </div>
+    </body>
+</html>
