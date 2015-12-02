634cc25f671c8ffd1cd146f8241b70d3828f9ef9
Rigterink <rigterinkcris@gmail.com>
2015-12-02 11:23:02 +0100

    slideshow toegevoegd
    slideshow toegevoegd nog niet helemaal af

--- Agenda.php
+++ Agenda.php
@@ -3,14 +3,14 @@
     <head>
 		<?php include 'head.php'; ?>
     </head>
-    <body>
+     <body>
     	<header>
-    	<?php include 'header.php'; ?>
-    	</header>
-        <div class="info">
-            <div class="agenda">
-        <iframe src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=grey&amp;src=j3ft4po2nu6490gg7q8g67dmjk%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=Europe%2FAmsterdam" style="border-width:0" width="600" height="600" frameborder="0" scrolling="no"></iframe>
-    </div>
-    </div>
+    	   <?php include 'header.php'; ?>
+    	   </header>
+             <div class="info">
+                 <div class="agenda">
+                     <iframe src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=grey&amp;src=j3ft4po2nu6490gg7q8g67dmjk%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=Europe%2FAmsterdam" style="border-width:0" width="600" height="600" frameborder="0" scrolling="no"></iframe>
+            </div>
+        </div>
     </body>
 </html>
