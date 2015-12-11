77eafe0caa008c80fae6230b70bd932c67e486ed
sebastiaan_vries <vries.sebastiaan@gmail.com>
2015-12-11 14:09:57 +0100

    Berichtjes toegevoegd

--- index.php
+++ index.php
@@ -12,7 +12,43 @@
             <a href="materiaaloverzicht.php">Materiaaloverzicht</a> <br> 
             <a href="materiaalperspeler.php">Materiaal per speler</a> <br>
             <a href="inschrijf.php">Inschrijven evenementen</a> <br>
-            <a href="eventmaken.php">Plan een nieuwe markt in</a>    
+            <a href="eventmaken.php">Plan een nieuwe markt in</a>
+
+            <h3>Welkom</h3> 
+                <p>Welkom bij Levende Historie Harderwijk (LHH).
+
+                    <p>LHH is een vereniging, die zich ten doel stelt het leven in de Hanzetijd uit te beelden.
+                    Zij doet dat door op evenementen uit de Hanzetijd of bij evenementen, die een middeleeuws 
+                    karakter hebben, een jaarmarkt neer te zetten. De jaarmarkt is voor het publiek vrijelijk 
+                    toegankelijk: de bezoeker mengt zich tussen de middeleeuwse karakters, die door spelers 
+                    van LHH worden uitgebeeld en de bezoeker beleeft een markt zoals die zo’n 4,5 eeuw geleden 
+                    heeft plaatsgevonden. Op de markt zijn verschillende kramen, die waren aan de man proberen 
+                    te brengen. Zo kun je denken  aan kleding, snuisterijen, zalfjes en bijvoorbeeld Mede, een 
+                    middeleeuwse honingdrank. Ben je niet in het bezit van de juiste geldstukken, dan kun je 
+                    meestal wel bij de geldwisselaar terecht, die je kan voorzien van gelderse munten. Op de 
+                    markt kun je de schout tegenkomen met z’n gemalin of met z’n rakkers. Ook wordt er muziek 
+                    gemaakt en als de spelers zich hebben warmgedraaid, wordt er meestal wel een middeleeuws 
+                    dansje ingezet, waarbij de bezoekers of degenen die dat willen, enkele pasjes mee kunnen 
+                    dansen. </p>
+
+                    <p>Ook in de middeleeuwen was niet alles koek en ei: het gebeurt vaak, dat een dief zich niet 
+                    aan de middeleeuwse regels houdt en terecht moet staan ten overstaan van de schout, die de 
+                    schuldige een gepaste straf oplegt.</p>
+
+                    <p>Kortom, er is altijd wat te doen op de markt van LHH. Het open karakter is één van de 
+                    speciale kenmerken van de middeleeuwse markt van LHH: de bezoeker begeeft zich tussen de 
+                    marktlieden en laat zich meevoeren met de middeleeuwse gewoonten. Veel plezier in Harderwijk,
+                    of waar je ons ook tegenkomt! </p>
+
+                    <p>LHH is in eerste instantie opgericht ter ondersteuning van de plaatselijke Hanzemarkten en 
+                    het Hanzefestival in Harderwijk. Inmiddels is de groep ook in te zetten op andere plaatsen. 
+                    Zie linkje Interesse?</p>
+
+                    <p>Naast het Hanzeverleden staat ook het tijdperk van Dickens in de belangstelling van de 
+                    spelers van LHH. In de laatste 2 weken van het jaar worden de victoriaanse kledingstukken 
+                    uit de mottenbollen gehaald, worden de personages onderling verdeeld en gaan de spelers naar 
+                    verschillende Christmas Carols gelegenheden.</p>
+                </p>   
         </div>
         
     </body>
