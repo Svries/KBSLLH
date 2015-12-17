5252c86564cca0e6e000b54557ef3be668fcf8ad
Amanda <bruintjeteddy@hotmail.com>
2015-12-17 11:15:39 +0100

    nieuws

--- Nieuws.php
+++ Nieuws.php
@@ -10,27 +10,23 @@
         </header>
 
         <div class="info">
+                            <div id="central" name= "Nieuws">
+                                <a href = "Nieuws.php?gb=true">Nieuw bericht</a>
+                                <hr>
+                                <?php
+                                if (isset($_GET['gb'])){
+                                    echo"
+                                    <form action = '' method = 'POST'>
+                                    Bericht:<br>
+                                    <textarea cols= '40' rows= '3' name= 'nieuws'></textarea><br>
+                                    <input type= 'submit' name= 'Toevoegen'>
+                                    </form>
+                                    ";
+                                    }else{
 
-            <div id="wrapper"> <!--This is the Div 1 in the picture-->
-                            <div id="central"> <!--This is the Div 3 in the picture-->
-        <div id="top3"> <!--This is the Div 7 in the picture-->
-</div>
-    </div><!--Closing "central"/Div 3-->
-          <div id="topBar"> <!--This is the Div 2 in the picture-->
-            <div id="NieuweTekst"> <input type= "text"</div> <!--This is the Div 5 in the picture-->
-            <div id="login"><!--This is the Div 6 in the picture-->
-            <div class="loginElement"><input type="text" id="txtUsername" class="loginElementBox" size="29"  value="User name" onClick="usernameFocus()" onBlur="usernameBlur()" /></div>               
-            <div class="loginElement">
-                <input type="text" name="txtFakePassword" id="txtFakePassword" class="loginElementBox" value="Password" onfocus="passwordFocus()" /><input style="display:none;" type="password" id="txtPassword" class="loginElementBoxFocus" onBlur="passwordBlur()" />
-            </div>
-
-            <input type="button" id="btnLogin" value="" onclick="login()"><br />
-
-            <span id="lblError"></span>
-        </div><!--Closing "login"-->
-    </div><!--Closing "topBar"-->
-</div><!--Closing "wrapper"-->
-
+                                    };
+                                    ?>
+                            </div><!--Closing "central"/Div 3-->
         </div>
     </body>
 </html>
