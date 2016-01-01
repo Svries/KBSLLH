9419ad2c5ab7f004c73350eef2aee1b54243a501
Amanda <bruintjeteddy@hotmail.com>
2016-01-01 15:53:55 +0100

    nieuws

--- Nieuws.php
+++ Nieuws.php
@@ -17,13 +17,46 @@
                                 if (isset($_GET['gb'])){
                                     echo"
                                     <form action = '' method = 'POST'>
+                                    Titel:<br>
+                                    <input type= 'text' name = 'titel'>
                                     Bericht:<br>
                                     <textarea cols= '40' rows= '3' name= 'nieuws'></textarea><br>
-                                    <input type= 'submit' name= 'Toevoegen'>
+                                    <input type= 'submit' name= 'submitBtn'>
                                     </form>
                                     ";
+                                    if(isset($_POST['submitBtn'])){
+                                        //varibelen
+                                        $titel = $_POST['titel'];
+                                        $nieuws = $_POST['nieuws'];
+                                        $date = date("Y-m-d");
+                                        include("nieuwsdb.php");
+                                        //sql
+                                        $sql = "INSERT INTO entries(titel, nieuws, date) VALUES ('$titel', '$nieuws', '$date')";
+                                        $stmt = $db->query($sql);
+                                        $stmt->execute();
+                                        echo "Uw nieuwsbericht is geplaatst.";
+                                    }
                                     }else{
-
+                                        //database betrekken
+                                        include("nieuwsdb.php");
+                                        //SQL
+                                        $sql = "SELECT * FROM entries ORDER BY id DESC";
+                                        $stmt = $db->Query ($sql);
+                                        $stmt->Execute();
+                                        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                                        if($stmt->rowCount() > 0){
+                                            while($row = $stmt->fetch()){
+                                                echo "
+                                                <b>Titel: </b>".$row['titel']."<br>
+                                                <b>Date: </b>".$row['date']."<br>
+                                                <b>Bericht:</b><br>
+                                                <p>
+                                                ".$row['nieuws']."
+                                                </p>
+                                                <hr>
+                                                ";
+                                            }
+                                        }
                                     };
                                     ?>
                             </div><!--Closing "central"/Div 3-->
