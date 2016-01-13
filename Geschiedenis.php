1fa7e842b003b3a9679274353f43eec1985d0e43
jelmerdejeger <jelmerdejeger@gmail.com>
2016-01-14 00:26:57 +0100

    Geshiedenis werkt weer

--- Geschiedenis.php
+++ Geschiedenis.php
@@ -15,44 +15,28 @@
         	<?php include 'header.php'; ?>
         </header>
         <div class="info">
-            
-            <h2>De Hanze-geschiedenis van Harderwijk</h2>
-            
-            <div class="vakgeschiedenis">
-                <div class="tekstvakgeschiedenis">
-                    In het begin van de 13e eeuw kwam Harderwijk in het bezit van stadsrechten. 
-                    De ligging aan de Zuiderzee maakte de stad een ideaal handelsknooppunt en een interessante stad voor het Hanzeverbond, 
-                    dat aan het einde van de 13e eeuw werd opgericht. 
-                    Samen met andere steden in de omgeving, zoals Deventer, Zutphen, Arnhem en Nijmegen werd het Gelders Hanzeverbond gevormd, 
-                    dat op zich weer deel uitmaakte van de band van Nederlandse Hanzesteden.
-                    De invloed van en in het Hanzeverbond, is ook gezien het magere achterland van Harderwijk niet erg groot geweest.
-                    
-                </div> 
-            
-                <div class='hardewijk'>
-                    <a class="fancybox" rel="group" href="images/1952.jpg"><img src="/images/1952.jpg" width="300,5" height="223"></a>
-                </div>
-            </div>
+        <?php 
+        require_once 'connection.php';
+            // haalt het artikel op
+            $query = $con->prepare("SELECT id, titel, bericht FROM text where titel = 'geschiedenis'");
+            $query->execute();
+            $artikel = $query->fetchAll();
+            $con = NULL;
 
-            <h2>De jongere geschiedenis</h2>
-
-            <div class="vakgeschiedenis">
-                <div class="tekstvakgeschiedenis">
-                    In de jongere geschiedenis van Harderwijk zijn met name de periode van het werfdepot tbv de Overzeese gebiedsdelen het noemen waard.
-                    In het begin van de 20e eeuw werd Harderwijk een garnizoensstad, nadat het werfdepot was gesloten. Na het afsluiten van de Zuiderzee 
-                    verdween ook de visserij goeddeels uit Harderwijk en gingen vissers zich op andere inkomstenbronnen toeleggen.
-
-                    
-                </div> 
-            
-                <div class='hardewijk'>  
-                    <a class="fancybox" rel="group" href="images/1952.jpg"><img src="/images/1952.jpg" width="300,5" height="223"></a>
-                </div>
-            </div>
-        </div>
-
-
-
+            foreach($artikel as $item) {
+                print $item['bericht'];
+            }
+            if(isset( $_SESSION['user_id'] ))
+                // kijkt of de user is ingelogd
+                    {
+                        if($_SESSION['user_type'] == 'admin')
+                        // kijkt of de user een administrator is, zoja dan laat hij de knop bewerken zien
+                        {
+                            print("<td><a href=\"artiekelbewerk.php?id=" . $item["id"] . "\">Bewerken</a></td>");
+                        }
+                    }
+    
+        ?>
 
         <!-- Add jQuery library -->
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
